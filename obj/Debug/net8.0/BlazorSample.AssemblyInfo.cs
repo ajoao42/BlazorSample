cafa404ef9d68b6d5accd1ae7307d3d33b660b9e
Antonio Joao <235779409+ajoao42@users.noreply.github.com>
2025-11-01 21:11:50 +0000

    Update 01-11-025 21:11

--- obj/Debug/net8.0/BlazorSample.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorSample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorSample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57a22cbeb4da31da55fa300ad77fb5725c0f6a07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5c76d341653b790cddc43fb85018dc87c06db95")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorSample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorSample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
